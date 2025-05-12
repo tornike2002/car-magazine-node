b63de4553cd1d70f7b35feaac98ebec59455bbeb
tornike2002 <toko.toka2000@gmail.com>
2025-05-12 04:52:05 +0400

    add register login and logout model validator and controller

---
+++ src/controllers/auth.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from 'express'
+import Auth from '../model/Auth'
+import bcrypt from 'bcryptjs'
+import generateToken from '../utils/jwt'
+
+export const register = async (req: Request, res: Response) => {
+  const { username, email, password } = req.body
+
+  const user = await Auth.findOne({ email })
+
+  if (user) {
+    return res.status(400).json({ message: 'User already exists' })
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const newUser = await Auth.create({
+    username,
+    email,
+    password: hashedPassword,
+  })
+
+  const token = generateToken(newUser._id.toString())
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    sameSite: 'lax',
+  })
+
+  res.status(201).json({ message: 'User created successfully' })
+}
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  const existingUser = await Auth.findOne({ email })
+
+  if (!existingUser) {
+    return res.status(400).json({ message: 'User not found' })
+  }
+
+  const passwordMatch = await bcrypt.compare(password, existingUser.password)
+
+  if (!passwordMatch) {
+    return res.status(400).json({ message: 'Invalid password' })
+  }
+
+  const token = generateToken(existingUser._id.toString())
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    sameSite: 'lax',
+  })
+
+  res.status(200).json({ message: 'User logged in successfully' })
+}
+
+export const logout = async (req: Request, res: Response) => {
+  res.clearCookie('token')
+  res.status(200).json({ message: 'User logged out successfully' })
+}
---
+++ src/model/Auth.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const authSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+)
+
+const Auth = mongoose.model('Auth', authSchema)
+
+export default Auth
