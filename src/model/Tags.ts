405c2377382cc01b1feb936dfe2bba72ea1219c0
tornike2002 <toko.toka2000@gmail.com>
2025-05-15 21:07:02 +0400

    add models for tags and category also validators

---
+++ src/model/Tags.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const tagSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+})
+
+const Tag = mongoose.model('Tag', tagSchema)
+
+export default Tag
